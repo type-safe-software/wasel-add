b300e99aa71bfea7d7565034b06effca9c1bbe63
karimadnan <karimmadnan@gmail.com>
2025-11-22 21:33:52 +0200

    chore: update logos & links

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,36 +2,60 @@
   <section class="h-full">
     <div class="flex justify-center items-center h-full flex-col text-primary">
       <img src="/images/wasel-add-logo.svg" alt="Discover Nuxt" />
-      <h1 class="x font-black text-4xl text-center mb-5">
+      <h1 class="x font-black text-2xl text-center mb-5 lg:text-4xl">
         Wasel Add is coming soon
       </h1>
       <span>Connect with us on social media</span>
 
       <div class="flex">
-        <img
-          src="/images/facebook-logo.svg"
-          alt="Connect with us on Facebook"
-          height="100"
-          width="100"
-        />
-        <img
-          src="/images/instagram-logo.svg"
-          alt="Connect with us on Instagram"
-          height="100"
-          width="100"
-        />
-        <img
-          src="/images/linkedin-logo.svg"
-          alt="Connect with us on LinkedIn"
-          height="100"
-          width="100"
-        />
-        <img
-          src="/images/whatsapp-logo.svg"
-          alt="Connect with us on Whatsapp"
-          height="100"
-          width="100"
-        />
+        <a
+          href="https://www.facebook.com/people/Wasel-Add/61581453611481/"
+          aria-label="Link to facebook page"
+          target="_blank"
+        >
+          <img
+            src="/images/facebook-logo.svg"
+            alt="Connect with us on Facebook"
+            height="100"
+            width="100"
+          />
+        </a>
+        <a
+          href="https://www.instagram.com/waseladd/"
+          aria-label="Link to Instagram page"
+          target="_blank"
+        >
+          <img
+            src="/images/instagram-logo.svg"
+            alt="Connect with us on Instagram"
+            height="100"
+            width="100"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/company/109139413/"
+          aria-label="Link to LinkedIn page"
+          target="_blank"
+        >
+          <img
+            src="/images/linkedin-logo.svg"
+            alt="Connect with us on LinkedIn"
+            height="100"
+            width="100"
+          />
+        </a>
+        <a
+          href="https://api.whatsapp.com/send/?phone=201031774437&text&type=phone_number&app_absent=0"
+          aria-label="Link to Whatsapp chat"
+          target="_blank"
+        >
+          <img
+            src="/images/whatsapp-logo.svg"
+            alt="Connect with us on Whatsapp"
+            height="100"
+            width="100"
+          />
+        </a>
       </div>
     </div>
   </section>
